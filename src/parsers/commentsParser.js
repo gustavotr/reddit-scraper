f3f31fe438d89fd19bd470f4354a7d6601d3496b
Gustavo T. Rudiger <gustavotrudiger@gmail.com>
2020-06-24 22:26:05 -0300

    fixup! Scrape comments

--- src/parsers/commentsParser.js
+++ src/parsers/commentsParser.js
@@ -3,11 +3,13 @@
 const Apify = require('apify');
 
 exports.commentsParser = async ({ page, request }) => {
+    await page.waitForSelector('[data-click-id=upvote] ~div');
     const data = await page.evaluate(() => {
         const numberOfVotes = document.querySelector('[data-click-id=upvote] ~div').innerHTML;
         const postedBy = document.querySelector('a[href^="/user/"]').innerHTML;
         const title = document.querySelector('h1').innerHTML;
         const text = document.querySelector('div[data-click-id=text]').innerText;
+
         return {
             numberOfVotes,
             postedBy,
@@ -19,35 +21,26 @@ exports.commentsParser = async ({ page, request }) => {
     const postUrl = request.url;
     const communityName = postUrl.match(/reddit\.com\/(.*)\/comments.*/)[1];
 
-    const comments = await page.evaluate(() => {
+    const comments = await page.$$eval('[id^=t1]', (elements) => {
         const temp = [];
-
-        $('[id^=t1]').each((index, el) => {
+        elements.forEach((el) => {
+            const span = Array.from($(el).find('span')).find((span) => $(span).text().includes('points'));
+            const points = span ? span.innerText.match(/(\d+).+/)[1] : null;
             const id = $(el).attr('id');
             const commentUrl = `${this.location.href}${id}`;
             const userName = $(el).find('a[href^="/user/"]').text();
-            const points = $(el).find('span');
-            // .filter((span) => $(span).text().includes('points')).match(/(\d+) points/)[1];
-            const commentDate = $(el).find(`CommentTopMeta--Created--${id}`).text();
+            const commentDate = $(el).find(`#CommentTopMeta--Created--${id}`).text();
             const description = $(el).find('[data-test-id="comment"]').text();
+            const comment = { commentUrl, userName, commentDate, description };
 
-            const comment = {
-                commentUrl,
-                userName,
-                points,
-                commentDate,
-                description,
-            };
-            console.log(comment);
+            if (points) {
+                comment.points = points;
+            }
+
             temp.push(comment);
         });
-
-        console.log(temp);
-
         return temp;
     });
-
-    await page.waitFor(100000000);
 
     const post = {
         postUrl,
@@ -55,6 +48,28 @@ exports.commentsParser = async ({ page, request }) => {
         ...data,
         comments,
     };
+    // const elements = $('[id^=t1]');
+    // const comments = [];
+
+    // for (const el of elements) {
+    //     const id = $(el).attr('id');
+    //     const commentUrl = `${this.location.href}${id}`;
+    //     const userName = $(el).find('a[href^="/user/"]').text();
+    //     const points = $(el).find('span');
+    //     // .filter((span) => $(span).text().includes('points')).match(/(\d+) points/)[1];
+    //     const commentDate = $(el).find(`CommentTopMeta--Created--${id}`).text();
+    //     const description = $(el).find('[data-test-id="comment"]').text();
+
+    //     const comment = {
+    //         commentUrl,
+    //         userName,
+    //         points,
+    //         commentDate,
+    //         description,
+    //     };
+    //     comments.push(comment);
+    // }
 
     await Apify.pushData(post);
+    await page.waitFor(1000000);
 };
