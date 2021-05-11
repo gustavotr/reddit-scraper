fb641e215605a8d8e65bf945225c5b2f75738c1f
Gustavo T. Rudiger <gustavo@trudax.tech>
2021-05-11 13:56:01 +0300

    fix: 🐛 increase scrolldown timeout

--- src/parsers/postsParser.js
+++ src/parsers/postsParser.js
@@ -2,28 +2,53 @@
 
 const Apify = require("apify");
 const { SCROLL_TIMEOUT } = require("../constants");
-const { splitUrl } = require("../tools");
+const { splitUrl, log } = require("../tools");
 
 exports.postsParser = async ({ requestQueue, page, request, maxPostCount }) => {
   let loading = true;
-  let previousPostLength = -1;
+  let previousPostLength = 0;
   let posts = [];
 
   setTimeout(() => {
     loading = false;
   }, SCROLL_TIMEOUT);
 
-  while (loading) {
-    await Apify.utils.puppeteer.infiniteScroll(page, { timeoutSecs: 1 });
-
-    posts = await page.$$eval("div.Post", (divs) =>
+  const getPostUrls = async () => {
+    const postUrls = await page.$$eval("div.Post", (divs) =>
       divs.map((el) => {
         const postUrl = $(el).find("a[data-click-id=timestamp]").attr("href");
         return postUrl;
       })
     );
+    return postUrls;
+  };
 
-    if (posts.length >= maxPostCount || previousPostLength === posts.length) {
+  const queueNewPosts = async (newPosts, userData) =>
+    newPosts.reduce(async (next, current) => {
+      await next;
+      return requestQueue.addRequest({
+        url: splitUrl(current),
+        userData,
+      });
+    }, Promise.resolve());
+
+  const hasReachedPostLimit = () => {
+    log.debug(`${posts.length} posts loaded`, {
+      maxPostCount,
+      previousPostLength,
+    });
+    return posts.length >= maxPostCount || previousPostLength === posts.length;
+  };
+
+  while (loading) {
+    await Apify.utils.puppeteer.infiniteScroll(page, {
+      timeoutSecs: 2,
+    });
+
+    posts = await getPostUrls(page);
+
+    if (hasReachedPostLimit()) {
+      log.debug("REACHED POST LIMIT");
       loading = false;
     }
 
@@ -32,10 +57,5 @@ exports.postsParser = async ({ requestQueue, page, request, maxPostCount }) => {
 
   posts.splice(maxPostCount);
 
-  for (const url of posts) {
-    await requestQueue.addRequest({
-      url: splitUrl(url),
-      userData: request.userData,
-    });
-  }
+  await queueNewPosts(posts, request.userData);
 };
